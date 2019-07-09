705154cd7f9e9951141960e0b8671264f49b258a
pawanmishra1 <pawan.mishra1@emc.com>
2019-07-09 00:38:50 -0700

    Add application files

---
+++ scripts/setup-clustern.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+##  Set hosts file
+
+echo "192.168.61.102  pks.emcdigital.lab      pks.emcdigital.lab" >> /etc/hosts
+
+
+##  Setup pks login
+   pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
+
+
+## Get PKS cluster Details
+  pks get-credentials pks-lab-cluster 
+
+#  - name: Get PKS Cluster configuration
+#    command: "'KUBECONFIG=cluster.config' pks get-credentials pks-lab-cluster "
+
+## Setup cluster details
+  kubectl config use-context pks-lab-cluster
+
+## Print cluster details
+  kubectl cluster-info
+
+
---
+++ scripts/setup-couchbase.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+##  Set hosts file
+
+echo "192.168.61.102  pks.emcdigital.lab      pks.emcdigital.lab" >> /etc/hosts
+
+
+##  Setup pks login
+   pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
+
+
+
+## Setup cluster details
+  kubectl config use-context pks-lab-cluster
+
+## Print cluster details
+  kubectl cluster-info
+
+## Setup CouchBase Database
+
+kubectl  apply -f app-setup/cluster-role.yaml
+
+kubectl  apply -f app-setup/operator.yaml
