d89a6efc629ae0863ddb77e5469e5fe9178361af
pawanmishra1 <pawan.mishra1@emc.com>
2019-07-10 02:38:11 -0700

    adding Test

---
+++ scripts/test-app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+##  Set hosts file
+
+echo "192.168.61.102  pks.emcdigital.lab      pks.emcdigital.lab" >> /etc/hosts
+
+
+echo "192.168.61.103  pks-lab-cluster.emcdigital.lab  pks-lab-cluster"  >> /etc/hosts
+
+##  Setup pks login
+   pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
+
+
+## Get PKS cluster Details
+  pks get-credentials pks-lab-cluster 
+
+
+## Setup cluster details
+  kubectl config use-context pks-lab-cluster
+
+## Print cluster details
+" echo "Funcational and Integration Test Completed"
+
+
