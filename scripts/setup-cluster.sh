132b0c0f8ced7d878fcda97d18491c3891de6899
pawanmishra1 <pawan.mishra1@emc.com>
2019-07-19 07:34:20 -0700

    adding

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -9,24 +9,25 @@ echo "192.168.61.103  pks-lab-cluster.emcdigital.lab  pks-lab-cluster"  >> /etc/
 
 
 ##  Setup pks login
-   pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
+pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
 
 ##Delete PKS Cluster
   ## pks delete-cluster pks-lab-cluster --non-interactive --wait
 
 ##  Create PKS Cluster
-   pks create-cluster pks-lab-cluster --external-hostname pks-lab-cluster.emcdigital.lab --plan small --num-nodes 3 --wait
+
+pks create-cluster pks-lab-cluster --external-hostname pks-lab-cluster.emcdigital.lab --plan small --num-nodes 3 --wait
 
 ## Get PKS cluster Details
-  pks get-credentials pks-lab-cluster 
+pks get-credentials pks-lab-cluster 
 
 #  - name: Get PKS Cluster configuration
 #    command: "'KUBECONFIG=cluster.config' pks get-credentials pks-lab-cluster "
 
 ## Setup cluster details
-  kubectl config use-context pks-lab-cluster
+kubectl config use-context pks-lab-cluster
 
 ## Print cluster details
-  kubectl cluster-info
+kubectl cluster-info
 
 
