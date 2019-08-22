1411ee33d7231fa066940dadfad4fe962d6a6fd4
pawanmishra <pawan.mishra1@emc.com>
2019-08-22 21:11:50 +0530

    add

--- scripts/setup-couchbase.sh
+++ scripts/setup-couchbase.sh
@@ -9,26 +9,26 @@ echo "192.168.61.102  pks.emcdigital.lab      pks.emcdigital.lab" >> /etc/hosts
 echo "192.168.61.103  pks-lab-cluster.emcdigital.lab  pks-lab-cluster"  >> /etc/hosts
 
 ##  Setup pks login
-   pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
+pks login -a pks.emcdigital.lab -u appmod -p Password@123 -k
 
 ##  Get PKS cluster Details
-  pks get-credentials pks-lab-cluster
+pks get-credentials pks-lab-cluster
 
 
 ##  Setup cluster details
-#  kubectl config use-context pks-lab-cluster
+kubectl config use-context pks-lab-cluster
 
 ##  Print cluster details
-#  kubectl cluster-info
+kubectl cluster-info
 
 ## Setup CouchBase Database Cluster
 
 
-#kubectl  apply -f resource-pks/app-setup/cluster-role.yaml
+kubectl  apply -f resource-pks/app-setup/cluster-role.yaml
 
 ## Setup CouchBase Database Client
 
-#kubectl  apply -f resource-pks/app-setup/operator.yaml
+kubectl  apply -f resource-pks/app-setup/operator.yaml
 
 
 ## Get Deployment Details 
